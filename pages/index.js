1179e6f66f597afee1913ae3b8d4147696e2c290
Sergei Smirnov <sergous@gmail.com>
2024-03-05 04:26:57 -0500

    Add tab navigation

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,49 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ tabBarIcon: makeIconRender("home") }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{ tabBarIcon: makeIconRender("cog") }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function makeIconRender(name) {
+  return ({ color, size }) => (
+    <MaterialCommunityIcons name={name} color={color} size={size} />
   );
 }
 
